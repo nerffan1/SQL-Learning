9437649ea45c8ab48eb0ece847d20f8718e97872
nerffan1 <ville2a@cmich.edu>
2026-04-25 22:49:13 -0400

    Remove bad queries

--- sql_queries/drug_warnings_years.sql
+++
@@ -1,8 +0,0 @@
--- This query lets you find out the number of warnings for different years
-SELECT
-warning_year,
-COUNT(*) AS year_counts
-FROM drug_warning
-GROUP BY warning_year
-ORDER BY year_counts DESC
-LIMIT 30;
